3b00b583bda6371c869eb7bc5cb9300aaa8c50a1
Dave Castaldo <djcastaldo@gmail.com>
2026-03-13 02:52:52 -0400

    keymap/layer updates; OSL_FNSYM keycode added

--- keyboards/womier/sk87/keymaps/djcastaldo/keymap.c
+++ keyboards/womier/sk87/keymaps/djcastaldo/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	LT(TMUX_LAYR,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, LT(EMO_LAYR,KC_BSLS), KC_DEL, KC_END, KC_PGDN,
 	TD(CAPS_LAYR), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NO, KC_ENT,
 	KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(RSFT_OSL),  KC_UP,
-	KC_LCTL, KC_LOPT, KC_LCMD, KC_NO,              KC_SPC,             KC_NO, TD(RCMD_OSL),TD(ROPT_OSL),TD(FN_OSL),KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+	KC_LCTL, KC_LOPT, KC_LCMD, KC_NO,              KC_SPC,             KC_NO, TD(RCMD_OSL),TD(ROPT_OSL),OSL_FNSYM,KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 //  [WIN_BASE]
 //,--------------------------------------------------------------------------------------------------------------,
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	LT(TMUX_LAYR,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, TD(DYN_LAYR), KC_DEL, KC_END, KC_PGDN,
 	TD(CAPS_LAYR), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NO, KC_ENT,
 	KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(RSFT_OSL),  KC_UP,
-	KC_LCTL, TD(LGUI_OSL), KC_LALT, KC_NO,           KC_SPC,          KC_NO, TD(RALT_OSL),KC_RWIN,TD(FN_OSL),KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+	KC_LCTL, TD(LGUI_OSL), KC_LALT, KC_NO,           KC_SPC,          KC_NO, TD(RALT_OSL),KC_RWIN,OSL_FNSYM,KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 //  [CLCK_LAYR]
 //,--------------------------------------------------------------------------------------------------------------,
