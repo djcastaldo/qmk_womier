6941077ebf5a668203e7e99ebb74fd8f3b7c2428
Dave Castaldo <djcastaldo@gmail.com>
2026-03-21 00:17:13 -0400

    add ability to use KC_F15 as a globe key

--- keyboards/womier/sk87/keymaps/djcastaldo/keymap.c
+++ keyboards/womier/sk87/keymaps/djcastaldo/keymap.c
@@ -55,16 +55,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //: |________||____||____||____||____||____||____||____||____||____||____||____||___________|        ____        :
 //: | Shift     || Z  || X  || C  || V  || B  || N  || M  || ,  || .  || /  ||TD(SFT_OSL)   |       |Up  |       :
 //: |___________||____||____||____||____||____||____||____||____||____||____||______________|  ____ |____| ____  :
-//: |Ctrl ||Opt  || Cmd ||                Space                ||TDCmd||TDOpt||TDFn ||Ctrl  | |Left||Down||Rigt| :
+//: |Ctrl ||Opt  || Cmd ||                Space                ||TDCmd||TDOpt||TDFn ||SRCtrl| |Left||Down||Rigt| :
 //: |_____||_____||_____||_____________________________________||_____||_____||_____||______| |____||____||____| :
 //`--------------------------------------------------------------------------------------------------------------`
     [MAC_BASE] = LAYOUT_ansi(
-	DUAL_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_F14, DUAL_SNAP, US_SIRI, AP_GLOB,
+	DUAL_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_F14, DUAL_SNAP, US_SIRI, KC_F15,
 	KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, BSPCFAST, KC_INS, KC_HOME, KC_PGUP,
 	LT(TMUX_LAYR,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, LT(EMO_LAYR,KC_BSLS), KC_DEL, KC_END, KC_PGDN,
 	TD(CAPS_LAYR), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NO, KC_ENT,
 	KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(RSFT_OSL),  KC_UP,
-	KC_LCTL, KC_LOPT, KC_LCMD, KC_NO,              KC_SPC,             KC_NO, TD(RCMD_OSL),TD(ROPT_OSL),OSL_FNSYM,KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+	KC_LCTL, KC_LOPT, KC_LCMD, KC_NO,              KC_SPC,             KC_NO, TD(RCMD_OSL),TD(ROPT_OSL),OSL_FNSYM,SP_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 //  [WIN_BASE]
 //,--------------------------------------------------------------------------------------------------------------,
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //: |________||____||____||____||____||____||____||____||____||____||____||____||___________|        ____        :
 //: | Shift     || Z  || X  || C  || V  || B  || N  || M  || ,  || .  || /  || TD(RSFT_OSL) |       |Up  |       :
 //: |___________||____||____||____||____||____||____||____||____||____||____||______________|  ____ |____| ____  :
-//: |LCtrl||TLGUI||LAlt ||                Space                ||TRAlt||RWin ||TDFn ||RCtrl | |Left||Down||Rigt| :
+//: |LCtrl||TLGUI||LAlt ||                Space                ||TRAlt||RWin ||TDFn ||SRCtrl| |Left||Down||Rigt| :
 //: |_____||_____||_____||_____________________________________||_____||_____||_____||______| |____||____||____| :
 //`--------------------------------------------------------------------------------------------------------------`
     [WIN_BASE] = LAYOUT_ansi(
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	LT(TMUX_LAYR,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, TD(DYN_LAYR), KC_DEL, KC_END, KC_PGDN,
 	TD(CAPS_LAYR), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NO, KC_ENT,
 	KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(RSFT_OSL),  KC_UP,
-	KC_LCTL, TD(LGUI_OSL), KC_LALT, KC_NO,           KC_SPC,          KC_NO, TD(RALT_OSL),KC_RWIN,OSL_FNSYM,KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+	KC_LCTL, TD(LGUI_OSL), KC_LALT, KC_NO,           KC_SPC,          KC_NO, TD(RALT_OSL),KC_RWIN,OSL_FNSYM,SP_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 //  [CLCK_LAYR]
 //,--------------------------------------------------------------------------------------------------------------,
