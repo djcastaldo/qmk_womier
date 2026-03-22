f2f0b23cf254ac6b3b3d22f091cbc2c8378d8e94
Dave Castaldo <djcastaldo@gmail.com>
2026-03-21 21:37:36 -0400

    add DYN_LT keycode, an alternative to TD(DYN_LAYR)

--- keyboards/womier/sk87/keymaps/djcastaldo/keymap.c
+++ keyboards/womier/sk87/keymaps/djcastaldo/keymap.c
@@ -74,9 +74,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //:  _______________________________________________________________________________________   ________________  :
 //: | `  || 1  || 2  || 3  || 4  || 5  || 6  || 7  || 8  || 9  || 0  || -  || =  || Bkspace | |Ins ||Home||PgUp| :
 //: |____||____||____||____||____||____||____||____||____||____||____||____||____||_________| |____||____||____| :
-//: | Tab   || Q  || W  || E  || R  || T  || Y  || U  || I  || O  || P  || [  || ]  || \    | |Del ||End ||PgDn| :
+//: |LT(Tab)|| Q  || W  || E  || R  || T  || Y  || U  || I  || O  || P  || [  || ]  ||DYN_LT| |Del ||End ||PgDn| :
 //: |_______||____||____||____||____||____||____||____||____||____||____||____||____||______| |____||____||____| :
-//: | Caps   || A  || S  || D  || F  || G  || H  || J  || K  || L  || ;  || '  || Enter     |                    :
+//: |TD(Caps)|| A  || S  || D  || F  || G  || H  || J  || K  || L  || ;  || '  || Enter     |                    :
 //: |________||____||____||____||____||____||____||____||____||____||____||____||___________|        ____        :
 //: | Shift     || Z  || X  || C  || V  || B  || N  || M  || ,  || .  || /  || TD(RSFT_OSL) |       |Up  |       :
 //: |___________||____||____||____||____||____||____||____||____||____||____||______________|  ____ |____| ____  :
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_BASE] = LAYOUT_ansi(
 	DUAL_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_LPAD, KC_PSCR, US_CATANA, CSTMTOG,
 	KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, BSPCFAST, KC_INS, KC_HOME, KC_PGUP,
-	LT(TMUX_LAYR,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, TD(DYN_LAYR), KC_DEL, KC_END, KC_PGDN,
+	LT(TMUX_LAYR,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, DYN_LT, KC_DEL, KC_END, KC_PGDN,
 	TD(CAPS_LAYR), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NO, KC_ENT,
 	KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(RSFT_OSL),  KC_UP,
 	KC_LCTL, TD(LGUI_OSL), KC_LALT, KC_NO,           KC_SPC,          KC_NO, TD(RALT_OSL),KC_RWIN,OSL_FNSYM,SP_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
